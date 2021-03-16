ee864b422afcf812fd583c97a45360da756148c9
Franco <francopanozzo@hotmail.com.ar>
2021-03-16 20:17:40 -0300

    Installed and implemented styled components

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  position: fixed;
+  width: 100vw;
+  padding: 15px 0;
+  background-color: red;
+`;
 
 export default function Navbar() {
-  return <nav>I'm navbar</nav>;
+  return <StyledNav>I'm navbar</StyledNav>;
 }
