f2ed1ed5e474a98611a86ee9b0738d1d03ade1e7
Franco <francopanozzo@hotmail.com.ar>
2021-03-27 22:51:59 -0300

    Added data fetching on the actor and director pages using the query strings

--- pages/actors/[id].js
+++ pages/actors/[id].js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default function ActorPage() {
-  return <div></div>;
+export default function ActorPage({ actor }) {
+  return <div>{actor.first_name + ' ' + actor.last_name}</div>;
+}
+
+export async function getServerSideProps({ params }) {
+  const { API_URL } = process.env;
+  const res_actor = await fetch(`${API_URL}/actors/${params.id}`);
+  const actor = await res_actor.json();
+
+  return {
+    props: { actor },
+  };
 }
--- pages/directors/[id].js
+++ pages/directors/[id].js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default function DirectorPage() {
-  return <div></div>;
+export default function DirectorPage({ director }) {
+  return <div>{director.first_name + ' ' + director.last_name}</div>;
+}
+
+export async function getServerSideProps({ params }) {
+  const { API_URL } = process.env;
+  const res_director = await fetch(`${API_URL}/directors/${params.id}`);
+  const director = await res_director.json();
+
+  return {
+    props: { director },
+  };
 }
