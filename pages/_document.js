a10b18c618326e345ad1377949da203b8f882d22
Franco <francopanozzo@hotmail.com.ar>
2021-03-28 17:40:33 -0300

    Added favicons

--- pages/_document.js
+++ pages/_document.js
@@ -33,6 +33,26 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&family=Roboto:wght@300;400;500&display=swap"
             rel="stylesheet"
           />
+
+          {/* Favicons */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest"></link>
         </Head>
         <body>
           <Main />
