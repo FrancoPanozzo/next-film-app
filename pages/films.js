3793c7376c47dd13c80e4cf2ec7af5488700d4fc
Franco <francopanozzo@hotmail.com.ar>
2021-03-31 20:12:18 -0300

    Added categories to the series and films pages

--- pages/films.js
+++ pages/films.js
@@ -1,3 +1,4 @@
+const qs = require('qs');
 import styled from 'styled-components';
 import fetch from 'isomorphic-unfetch';
 import CategoryCarousel from 'components/CategoryCarousel';
@@ -5,12 +6,19 @@ import Head from 'next/head';
 
 const StyledFilms = styled.main``;
 
-export default function Films({ films }) {
+export default function Films({ films, categories }) {
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
   return (
     <StyledFilms>
       <Head>
         <title>Notflix - Films</title>
       </Head>
+      {categories.map((c) => {
+        let title = capitalizeFirstLetter(`${c.title.toLowerCase()}`);
+        return <CategoryCarousel key={c.id} title={title} films={c.films} />;
+      })}
       <CategoryCarousel films={films} title="All" />
     </StyledFilms>
   );
@@ -21,7 +29,35 @@ export async function getServerSideProps() {
   const res_films = await fetch(`${API_URL}/films`);
   const films = await res_films.json();
 
+  // Get the categories with more than 1 serie
+  const query = qs.stringify({
+    _where: [{ films_gte: 5 }],
+  });
+  const res_categories = await fetch(`${API_URL}/categories?${query}`);
+  const categoriesOrdered = await res_categories.json();
+  const categories = shuffle(categoriesOrdered);
+
   return {
-    props: { films },
+    props: { films, categories },
   };
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
--- pages/series.js
+++ pages/series.js
@@ -1,3 +1,4 @@
+const qs = require('qs');
 import styled from 'styled-components';
 import fetch from 'isomorphic-unfetch';
 import CategoryCarousel from 'components/CategoryCarousel';
@@ -5,12 +6,19 @@ import Head from 'next/head';
 
 const StyledFilms = styled.main``;
 
-export default function Series({ series }) {
+export default function Series({ series, categories }) {
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
   return (
     <StyledFilms>
       <Head>
         <title>Notflix - Series</title>
       </Head>
+      {categories.map((c) => {
+        let title = capitalizeFirstLetter(`${c.title.toLowerCase()}`);
+        return <CategoryCarousel key={c.id} title={title} series={c.series} />;
+      })}
       <CategoryCarousel series={series} title="All" />
     </StyledFilms>
   );
@@ -21,7 +29,35 @@ export async function getServerSideProps() {
   const res_series = await fetch(`${API_URL}/series`);
   const series = await res_series.json();
 
+  // Get the categories with more than 1 serie
+  const query = qs.stringify({
+    _where: [{ series_gte: 5 }],
+  });
+  const res_categories = await fetch(`${API_URL}/categories?${query}`);
+  const categoriesOrdered = await res_categories.json();
+  const categories = shuffle(categoriesOrdered);
+
   return {
-    props: { series },
+    props: { series, categories },
   };
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
